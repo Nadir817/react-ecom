368d7c68942d0c3ce7fa3fe726280985d193915c
Nadir817 <nadir.erkin@gmail.com>
2020-01-09 19:30:46 -0500

    update shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shoppage/shoppage.component";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shoppage/shoppage.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+
+class ShopPage extends Component {
+  state = {
+    collections: SHOP_DATA
+  };
+  render() {
+    return (
+      <div>
+        {this.state.collections.map(({ id, ...otherCollectionProp }) => (
+          <CollectionPreview key={id} {...otherCollectionProp} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
